e92b0fb154010856d5d807161389d83ae804c49c
hosthorizonweblab-spec <host.horizonweblab@gmail.com>
2026-08-28 12:37:54 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -111,7 +111,7 @@ function renderMenu(categories, recommended, products) {
                             ${catProducts.map(p => `
                                 <div class="menu-card-item" data-variants="${p.variants ? JSON.stringify(p.variants).replace(/"/g, '&quot;') : '[]'}">
                                     <div class="menu-item-header">
-                                        <h3 class="menu-item-name">${p.name.replace(/\s*\((.*?)\)/, ' <span class="inline-desc">($1)</span>')} ${p.description ? `<span class="inline-desc">(${p.description})</span><em style="display:none;">${p.description}</em>` : ''}</h3>
+                                        <h3 class="menu-item-name">${p.name.replace(/\s*\((.*?)\)/g, ' <span class="inline-desc">($1)</span>')} ${p.description ? (p.description.length <= 40 ? `<span class="inline-desc">(${p.description})</span>` : '') + `<em style="display:none;">${p.description}</em>` : ''}</h3>
                                         <div class="menu-item-dots"></div>
                                         ${p.variants && p.variants.length > 0 ? 
                                             (function() {
